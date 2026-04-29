b8d127878c7ff5402ccfdad6bb59a74546245758
ayachi2225 <1728353049@qq.com>
2026-04-29 15:42:17 +0800

    Site updated: 2026-04-29 15:42:16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777448095001";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777448536493";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
